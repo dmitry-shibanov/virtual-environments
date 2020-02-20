dc90226b4b20f4053eaba41dda9fb2f473b96a45
Dmitry Shibanov <v-dmshib@microsoft.com>
2020-02-20 13:12:00 +0300

    add creation release

--- Images.CI/create-release.ps1
+++ Images.CI/create-release.ps1
@@ -1,19 +1,33 @@
 param(
-    [String] [Parameter (Mandatory = $True)] $ArtifactUrl,
-    [String] [Parameter (Mandatory = $True)] $BuildArtifactsPath,
+    [String] [Parameter (Mandatory = $True)] $BuildId,
     [String] [Parameter (Mandatory = $True)] $Organization,
-    [String] [Parameter (Mandatory = $True)] $Project
+    [String] [Parameter (Mandatory = $True)] $Project,
+    [String] [Parameter (Mandatory = $True)] $ImageName,
+    [String] [Parameter (Mandatory = $True)] $DefinitionId,
+    [String] [Parameter (Mandatory = $True)] $AccessToken
 )
 
-$Body = @{
-    definitionId = "3851"
-    variables = {
-        ImageBuildArtifactUrl = $ArtifactUrl
-        ImageName = "test-name"
-    }
-    isDraft = "false"
+$Body = @"
+{
+    "definitionId" : "$DefinitionId",
+    "variables" : {
+        "ImageBuildId" : {
+            "value" : "$BuildId"
+        },
+        "ImageName" : {
+            "value" : "$ImageName"
+        }
+    },
+    "isDraft" : "false"
+}
+"@
+
+$URL = "https://vsrm.dev.azure.com/$Organization/$Project/_apis/release/releases?api-version=5.1"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("'':${AccessToken}"))
+$headers = @{
+    Authorization = "Basic ${base64AuthInfo}"
 }
 
-$NewRelease = Invoke-WebRequest "https://vsrm.dev.azure.com/$Organization/$Project/_apis/release/releases?api-version=5.1" -Body $Body -Method "POST"
+$NewRelease = Invoke-RestMethod $URL -Body $Body -Method "POST" -Headers $headers -ContentType "application/json"
 
-Write-Host "Create new release at $($NewRelease.release._links.web.refs)"
+Write-Host "Created release: $($NewRelease.release._links.web.refs)"
